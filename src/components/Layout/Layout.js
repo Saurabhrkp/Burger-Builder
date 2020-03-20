8f268532d0ce67be187a9cab7d97f816975695b9
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2020-03-21 00:30:39 +0530

    Used Toolbar component and increased margin top

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 import Aux from '../../hoc/Auxiliary';
 import styles from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = props => {
   return (
     <Aux>
-      <div>Toolbar, SiderDrawer, Backdrop</div>
+      <Toolbar />
       <main className={styles.Content}>{props.children}</main>
     </Aux>
   );
