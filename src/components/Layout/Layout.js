c7e45ac0fb3405912833093fa309bf678ea289a0
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2020-03-21 13:16:50 +0530

    Made Layout a Functional Component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Aux from '../../hoc/Auxiliary';
 import styles from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = props => {
+const Layout = props => {
+  var [showSideDrawer, setShowSideDrawer] = useState(true);
+
+  const sideDrawerClosedHandler = () => {
+    setShowSideDrawer(false);
+  };
+
   return (
     <Aux>
       <Toolbar />
-      <SideDrawer />
+      <SideDrawer closed={sideDrawerClosedHandler} open={showSideDrawer} />
       <main className={styles.Content}>{props.children}</main>
     </Aux>
   );
 };
-export default layout;
+
+export default Layout;
