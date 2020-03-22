318a6c81e4c68cd36052f8aee2fe776f138389c4
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2020-03-23 00:48:49 +0530

    Converted Checkout as Stateful Class Component

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,19 +1,44 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 
-const Checkout = () => {
-  const [ingredients, useIngredients] = useState({
-    salad: 1,
-    meat: 1,
-    bacon: 1,
-    cheese: 1
-  });
-  return (
-    <div>
-      <CheckoutSummary ingredients={ingredients} />
-    </div>
-  );
-};
+class Checkout extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      meat: 1,
+      bacon: 1,
+      cheese: 1
+    }
+  };
+
+  componentDidMount() {
+    const query = new URLSearchParams(this.props.location.search);
+    const ingredients = {};
+    for (let params of query.entries()) {
+      ingredients[params[0]] = +params[1];
+    }
+    this.setState({ ingredients: ingredients });
+  }
+
+  checkoutCancelledHandler = () => {
+    this.props.history.goBack();
+  };
+
+  checkoutContinuedHandler = () => {
+    this.props.history.replace('/checkout/contact-data');
+  };
+  render() {
+    return (
+      <div>
+        <CheckoutSummary
+          ingredients={this.state.ingredients}
+          checkoutCancelled={this.checkoutCancelledHandler}
+          checkoutContinued={this.checkoutContinuedHandler}
+        />
+      </div>
+    );
+  }
+}
 
 export default Checkout;
