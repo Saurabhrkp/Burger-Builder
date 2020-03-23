16c621fb290b8bcdb138e8cdd3ea79692726dbf5
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2020-03-23 16:31:56 +0530

    Fetching Orders from Firebase

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react';
 
 import Order from '../../components/Order/Order';
+import axios from '../../axios-order';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component {
+  state = { orders: [], loading: true };
+  componentDidMount() {
+    axios
+      .get('/orders.json')
+      .then(response => {
+        const fetchedOrder = [];
+        for (let key in response.data) {
+          fetchedOrder.push({
+            ...response.data[key],
+            id: key
+          });
+        }
+        this.setState({ loading: false, orders: fetchedOrder });
+      })
+      .catch(error => {
+        this.setState({ loading: false });
+      });
+  }
   render() {
     return (
       <div>
-        <Order />
-        <Order />
+        {this.state.orders.map(order => (
+          <Order
+            key={order.id}
+            ingredients={order.ingredients}
+            price={order.price}
+          />
+        ))}
       </div>
     );
   }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
